f158400eb3fc80683e4f9ccd8ef4f63707786efe
Jonas Felipe Durau <jonasdurau@gmail.com>
2022-10-25 14:22:41 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tomara que dessa vez funcione.");
+        }
+    }
+}
